badb39dc637e246839fdac617a61746087d13670
mg3183 <mg3183@columbia.edu>
2022-01-18 00:12:34 -0500

    Implement basic gain control

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -13,9 +13,14 @@
 HumanizerAudioProcessorEditor::HumanizerAudioProcessorEditor (HumanizerAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    mGainSlider.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
+    mGainSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 50, 20);
+    mGainSlider.setRange(0.0f, 1.0f, 0.01f);
+    mGainSlider.setValue(0.5f);
+    mGainSlider.addListener(this);
+    addAndMakeVisible(mGainSlider);
+    
+    setSize (200, 300);
 }
 
 HumanizerAudioProcessorEditor::~HumanizerAudioProcessorEditor()
@@ -25,16 +30,18 @@ HumanizerAudioProcessorEditor::~HumanizerAudioProcessorEditor()
 //==============================================================================
 void HumanizerAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.fillAll(juce::Colours::lightcyan);
 }
 
 void HumanizerAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    mGainSlider.setBounds((getWidth() / 2) - 50, (getHeight() / 2) - 75, 100, 150);
+}
+
+void HumanizerAudioProcessorEditor::sliderValueChanged(juce::Slider *slider)
+{
+    if(slider == &mGainSlider)
+    {
+        audioProcessor.mGain = mGainSlider.getValue();
+    }
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -14,7 +14,8 @@
 //==============================================================================
 /**
 */
-class HumanizerAudioProcessorEditor  : public juce::AudioProcessorEditor
+class HumanizerAudioProcessorEditor  : public juce::AudioProcessorEditor,
+                                       public juce::Slider::Listener
 {
 public:
     HumanizerAudioProcessorEditor (HumanizerAudioProcessor&);
@@ -23,10 +24,12 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+    
+    void sliderValueChanged (juce::Slider* slider) override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
+    juce::Slider mGainSlider;
+
     HumanizerAudioProcessor& audioProcessor;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (HumanizerAudioProcessorEditor)
